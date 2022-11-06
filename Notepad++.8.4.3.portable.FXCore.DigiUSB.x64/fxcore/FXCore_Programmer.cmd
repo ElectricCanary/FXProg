c07fa55fa04a01cc58ed28ea7724dd6933d3b40a
Antoine Ricoux <52302192+ElectricCanary@users.noreply.github.com>
2022-11-06 21:03:57 +0100

    Delete Notepad++.8.4.3.portable.FXCore.DigiUSB.x64 directory

--- Notepad++.8.4.3.portable.FXCore.DigiUSB.x64/fxcore/FXCore_Programmer.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-"%~dp0FXCore_Programmer.exe" %1 %2 %3 %4
-if %errorlevel% EQU 0 (echo. & echo NO ERRORS ) Else ( echo. & echo ERROR FAILED &color CF )
-pause
--- Notepad++.8.4.3.portable.FXCore.DigiUSB.x64/fxcore/assemble.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-"%~dp0FXCoreCmdAsm.exe" %1 %2 %3 %4 %5
-if %errorlevel% EQU 0 (echo. & echo NO ERRORS ) Else ( echo. & echo ERROR FAILED &color CF )
-pause
