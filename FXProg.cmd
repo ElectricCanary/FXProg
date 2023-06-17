b4d2c15d3228b0148586f7517ffbf31c1a74070d
Antoine Ricoux <52302192+ElectricCanary@users.noreply.github.com>
2023-06-18 01:13:06 +0200

    Update and rename FXCore_Programmer.cmd to FXProg.cmd

--- FXProg.cmd
+++ FXProg.cmd
@@ -1,4 +1,4 @@
 @echo off
-"%~dp0FXCore_Programmer.exe" %1 %2 %3 %4
+"%~dp0FXProg.exe" %1 %2 %3 %4
 if %errorlevel% EQU 0 (echo. & echo NO ERRORS ) Else ( echo. & echo ERROR FAILED &color CF )
 pause
